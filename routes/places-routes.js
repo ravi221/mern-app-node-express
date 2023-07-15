779604a1d5fff38d7240f8713d455be84be43929
Ravi K <mrravi221@gmail.com>
2023-07-15 20:55:57 +0530

    changes

---
+++ routes/places-routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { check } = require("express-validator");
+
+const placesControllers = require("../controllers/places-controller");
+
+const router = express.Router();
+
+router.get("/:pid", placesControllers.getPlaceById);
+
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
+
+router.post("/", [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+
+], placesControllers.createPlace);
+
+router.patch("/:pid", placesControllers.updatePlace);
+
+router.delete("/:pid", placesControllers.deletePlace);
+
+module.exports = router;
